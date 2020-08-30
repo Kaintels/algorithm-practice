92bbea3d47717d4fbda49a45c7da6bf37e57552e
Seungwoo Han <swoohan@outlook.kr>
2020-08-31 02:08:28 +0900

    A×B

---
+++ 10998.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+"""
+
+a, b = map(int, input().split())
+
+
+def mul(a, b):
+    num = a * b
+    return num
+
+
+c = mul(a, b)
+print(c)
