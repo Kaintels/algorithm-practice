21a9ff77e02a40c75d94aadac3abab9a8602a780
Seungwoo Han <swoohan@outlook.kr>
2021-04-26 01:21:39 +0900

    정수 N개의 합

--- 15596.py
+++ 15596.py
@@ -6,7 +6,7 @@
 a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
 리턴값: a에 포함되어 있는 정수 n개의 합 (정수)
 '''
-def solve(a: list) -> int::
+def solve(a: list) -> int:
     ans = 0
     for i in a:
         ans += i
